81a1652c88b648c612a8bac5cf6fc0a48a45309d
Rafael-S-Dias <161033176+Rafael-S-Dias@users.noreply.github.com>
2025-03-19 11:16:14 -0300

    Add files via upload

---
+++ 19-03/Try_Catch/Contas_Bancarias/Conta.java
@@ -0,0 +1,7 @@
+package Try_Catch_Aritimetica.Contas_Bancarias;
+
+public class Conta {
+   private String numeroConta;
+   private String nomeTitular;
+   private float saldo; 
+}
---
+++ 19-03/Try_Catch/Contas_Bancarias/Excecoes.java
@@ -0,0 +1,11 @@
+package Try_Catch_Aritimetica.Contas_Bancarias;
+
+public class Excecoes {
+
+    public class ExcecaoDadoInvalido extends Exception {
+        public ExcecaoDadoInvalido(String mensagem){
+            super(mensagem);
+        }
+}
+
+}
