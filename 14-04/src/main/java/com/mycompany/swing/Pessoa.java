4612676f506d48fcc02f03d351d8421c3c81e0ab
Rafael-S-Dias <161033176+Rafael-S-Dias@users.noreply.github.com>
2025-04-14 11:34:45 -0300

    Add files via upload

---
+++ 14-04/src/main/java/com/mycompany/swing/Pessoa.java
@@ -0,0 +1,43 @@
+package com.mycompany.swing;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aluno.den
+ */
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" + "nome=" + nome + ", idade=" + idade + '}';
+    }
+    
+    
+}
