0af0afb725b6910600a7042d37345029ec34b123
Rafael-S-Dias <161033176+Rafael-S-Dias@users.noreply.github.com>
2025-04-23 11:28:39 -0300

    Add files via upload

---
+++ NetBeansProjects/23-04_Atividade/src/main/java/com/mycompany/_atividade/App.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._atividade;
+
+import view.Login;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+                
+    }
+}
