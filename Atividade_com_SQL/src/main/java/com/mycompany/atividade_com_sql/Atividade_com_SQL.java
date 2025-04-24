30c64c41cca9decf66c469d5ae8ab69b394e72a2
Rafael-S-Dias <161033176+Rafael-S-Dias@users.noreply.github.com>
2025-04-24 11:25:07 -0300

    Add files via upload

---
+++ Atividade_com_SQL/src/main/java/com/mycompany/atividade_com_sql/Atividade_com_SQL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade_com_sql;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class Atividade_com_SQL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
